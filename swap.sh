3a7061e75ae4cb9fb4620227027226e27ee846de
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 18:15:09 +0600

    Existence of the files are now checked.

--- swap.sh
+++ swap.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-temp_name1="_an_unexpected_name_.txt"
-temp_name2="_another_unexpected_name_.txt"
 
-mv $1 $temp_name1
-mv $2 $temp_name2
+if [ $# -ne 2 ]; then
+	exit_code=1 #Invalid number of arguments
+	echo "Invalid number of arguments."
+elif [ ! -f $1 ] || [ ! -f $2 ]; then
+	exit_code=2 #At least one file does not exist
+    echo "File not found!"
+else
+	exit_code=0
+	
+	temp_name1="_an_unexpected_name_.txt"
+	temp_name2="_another_unexpected_name_.txt"
 
-mv $temp_name1 $2
-mv $temp_name2 $1
+	mv $1 $temp_name1
+	mv $2 $temp_name2
 
-exit 0
+	mv $temp_name1 $2
+	mv $temp_name2 $1
+fi
+
+exit $exit_code
