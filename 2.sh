b937944ac43139b203d78e429a2b988db7e2538a
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 16:59:24 +0600

    It works somewhat...

--- 2.sh
+++ 2.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-
+OIFS="$IFS"
+IFS=$'\n'
+for file in `find . -type f -name "*.cpp"`
+do
+	chmod 444 $file # Make read-only
+	mv "$file" "$(basename "$file" .cpp).c" # Change extention
+done
+IFS="$OIFS"
