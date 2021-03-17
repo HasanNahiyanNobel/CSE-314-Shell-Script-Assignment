7b231c0ebcf2365c14aef64232c6a6585482a056
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 17:57:35 +0600

    Some optimizations.

--- swap.sh
+++ swap.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-file1=$1
-file2=$2
-
 temp_name1="_an_unexpected_name_.txt"
 temp_name2="_another_unexpected_name_.txt"
 
-mv $file1 $temp_name1
-mv $file2 $temp_name2
+mv $1 $temp_name1
+mv $2 $temp_name2
 
 mv $temp_name1 $2
 mv $temp_name2 $1
