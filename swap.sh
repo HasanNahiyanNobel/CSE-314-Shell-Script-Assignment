af1dd6d8ea06eb96d28b2651e06b6ae9b80e848a
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 19:23:21 +0600

    All the 5 conditions seem to work fine.

--- swap.sh
+++ swap.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-exit_if_previous_command_was_unsuccessful()
+exit_with_code_5()
 {
-	echo "Entered"
-	if [ ! $? -eq 0 ]; then #Previous command was not successfull
-		exit_code=5 #Some other error occurred
-		echo "Some other error occurred."
-		exit $exit_code
-	fi
+	echo "Some other error occurred."
+	exit 5
 }
 
 
@@ -26,7 +22,7 @@ elif [ ! -f $1 ] || [ ! -f $2 ]; then
 	echo "At least one of the files is not regular."
 	
 	
-elif ([ ! -r $1 ] && [ ! -w $1 ]) || ([ ! -r $2 ] && [ ! -w $2 ]); then
+elif ([ ! -r $1 ] || [ ! -w $1 ]) || ([ ! -r $2 ] || [ ! -w $2 ]); then
 	exit_code=4 #At least one of the files is not readable and writeable
 	echo "At least one of the files is not readable and writeable."
 	
@@ -35,17 +31,21 @@ else
 	temp_name1="_an_unexpected_name_.txt"
 	temp_name2="_another_unexpected_name_.txt"
 
-	mv $1 $temp_name1
-	exit_if_previous_command_was_unsuccessful
+	if ! mv $1 $temp_name1 ; then
+		exit_with_code_5		
+	fi
 	
-	mv $2 $temp_name2
-	exit_if_previous_command_was_unsuccessful
+	if ! mv $2 $temp_name2 ; then
+		exit_with_code_5
+	fi
 	
-	mv $temp_name1 $2
-	exit_if_previous_command_was_unsuccessful
+	if ! mv $temp_name1 $2 ; then
+		exit_with_code_5
+	fi
 	
-	mv $temp_name2 $1
-	exit_if_previous_command_was_unsuccessful
+	if ! mv $temp_name2 $1 ; then
+		exit_with_code_5
+	fi
 	
 	exit_code=0
 	echo "Swapped successfully."
