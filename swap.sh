a90585f016441188df6dbac822a9dd4f238b8f9d
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 19:01:52 +0600

    Tried to handle other errors.

--- swap.sh
+++ swap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+exit_if_previous_command_was_unsuccessful()
+{
+	echo "Entered"
+	if [ ! $? -eq 0 ]; then #Previous command was not successfull
+		exit_code=5 #Some other error occurred
+		echo "Some other error occurred."
+		exit $exit_code
+	fi
+}
+
 
 if [ $# -ne 2 ]; then
 	exit_code=1 #Invalid number of arguments
@@ -21,18 +31,23 @@ elif ([ ! -r $1 ] && [ ! -w $1 ]) || ([ ! -r $2 ] && [ ! -w $2 ]); then
 	echo "At least one of the files is not readable and writeable."
 	
 	
-else
-	exit_code=0
-	
+else	
 	temp_name1="_an_unexpected_name_.txt"
 	temp_name2="_another_unexpected_name_.txt"
 
 	mv $1 $temp_name1
-	mv $2 $temp_name2
-
-	mv $temp_name1 $2
-	mv $temp_name2 $1
+	exit_if_previous_command_was_unsuccessful
 	
+	mv $2 $temp_name2
+	exit_if_previous_command_was_unsuccessful
+	
+	mv $temp_name1 $2
+	exit_if_previous_command_was_unsuccessful
+	
+	mv $temp_name2 $1
+	exit_if_previous_command_was_unsuccessful
+	
+	exit_code=0
 	echo "Swapped successfully."
 fi
 
