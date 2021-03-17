101664f38f28d1f1eb922a46cbf4c0eb202affb1
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 17:50:45 +0600

    Swapping now works.

--- swap.sh
+++ swap.sh
@@ -3,6 +3,11 @@
 file1=$1
 file2=$2
 
-tempooo=$file1
-mv $file1 $file2
-mv $file2 $tempooo
+temp_name1="_an_unexpected_name_.txt"
+temp_name2="_another_unexpected_name_.txt"
+
+mv $file1 $temp_name1
+mv $file2 $temp_name2
+
+mv $temp_name1 $2
+mv $temp_name2 $1
