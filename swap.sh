5a5b8a62927ae7a8272278d74541fb25ed24ba94
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 18:33:17 +0600

    Checked whether the files are readable and writeable.

--- swap.sh
+++ swap.sh
@@ -4,12 +4,23 @@
 if [ $# -ne 2 ]; then
 	exit_code=1 #Invalid number of arguments
 	echo "Invalid number of arguments."
+	
+	
 elif [ ! -e $1 ] || [ ! -e $2 ]; then
 	exit_code=2 #At least one file does not exist
     echo "At least one file does not exist."
+    
+    
 elif [ ! -f $1 ] || [ ! -f $2 ]; then
 	exit_code=3 #At least one of the files is not regular
 	echo "At least one of the files is not regular."
+	
+	
+elif ([ ! -r $1 ] && [ ! -w $1 ]) || ([ ! -r $2 ] && [ ! -w $2 ]); then
+	exit_code=4 #At least one of the files is not readable and writeable
+	echo "At least one of the files is not readable and writeable."
+	
+	
 else
 	exit_code=0
 	
@@ -21,6 +32,8 @@ else
 
 	mv $temp_name1 $2
 	mv $temp_name2 $1
+	
+	echo "Swapped successfully."
 fi
 
 exit $exit_code
