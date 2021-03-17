18c771487915406396d21020708ba4c830bd26bb
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 17:15:47 +0600

    Now it works, but not recursively.

--- 2.sh
+++ 2.sh
@@ -2,9 +2,9 @@
 
 OIFS="$IFS"
 IFS=$'\n'
-for file in `find . -type f -name "*.cpp"`
+for file in *.cpp
 do
-	chmod 444 $file # Make read-only
-	mv "$file" "$(basename "$file" .cpp).c" # Change extention
+	chmod 444 $file
+	mv "$file" "${file%.cpp}.c"
 done
 IFS="$OIFS"
