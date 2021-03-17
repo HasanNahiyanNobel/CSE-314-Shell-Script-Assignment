9215d1dc828ee02e0434c51852316f59e56b2655
Hasan Nahiyan Nobel <nobel.hasan.nahiyan@gmail.com>
2021-03-17 18:20:04 +0600

    Checked whether the files are regular.

--- swap.sh
+++ swap.sh
@@ -4,9 +4,12 @@
 if [ $# -ne 2 ]; then
 	exit_code=1 #Invalid number of arguments
 	echo "Invalid number of arguments."
-elif [ ! -f $1 ] || [ ! -f $2 ]; then
+elif [ ! -e $1 ] || [ ! -e $2 ]; then
 	exit_code=2 #At least one file does not exist
-    echo "File not found!"
+    echo "At least one file does not exist."
+elif [ ! -f $1 ] || [ ! -f $2 ]; then
+	exit_code=3 #At least one of the files is not regular
+	echo "At least one of the files is not regular."
 else
 	exit_code=0
 	
