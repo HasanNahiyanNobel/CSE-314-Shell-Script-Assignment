7b611f13475a4c3b0a0dad8213fa8978b8e300d7
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-03-23 13:50:14 +0600

    Successfully changed .cpp extensions to .c recursively.

--- Problem 2/2.sh
+++ Problem 2/2.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-OIFS="$IFS"
-IFS=$'\n'
-for file in *.cpp
-do
-	chmod 444 $file
-	mv "$file" "${file%.cpp}.c"
-done
-IFS="$OIFS"
+# Concept taken from:
+## For recursive function: https://unix.stackexchange.com/a/494146
+## For current directory path: https://stackoverflow.com/a/246128
+
+number_of_cpp_files=0
+
+walk_dir () {	
+	# shopt -s nullglob dotglob
+
+	for pathname in "$1"/*
+	do
+		if [ -d "$pathname" ]; then # $pathname is a directory, do the same thing recursively
+			walk_dir "$pathname"
+		else # $pathname is a file
+			case "$pathname" in
+				*.cpp)	mv -- "$pathname" "${pathname%.cpp}.c"
+						number_of_cpp_files=`expr $number_of_cpp_files + 1`;;
+			esac
+		fi
+	done
+}
+
+directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+walk_dir "$directory"
+
+echo "Total count of .cpp files = $number_of_cpp_files"
+
