ae457bb70b19b27513dedd340ea9f434a9fa559e
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-03-23 14:09:03 +0600

    All the renamed .cpp files are now also made read-only.

--- Problem 2/2.sh
+++ Problem 2/2.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# Concept taken from:
-## For recursive function: https://unix.stackexchange.com/a/494146
-## For current directory path: https://stackoverflow.com/a/246128
-
 number_of_cpp_files=0
 
-walk_dir () {	
-	# shopt -s nullglob dotglob
-
+walk_dir () { # Recursive concept from https://unix.stackexchange.com/a/494146
 	for pathname in "$1"/*
 	do
 		if [ -d "$pathname" ]; then # $pathname is a directory, do the same thing recursively
 			walk_dir "$pathname"
 		else # $pathname is a file
 			case "$pathname" in
-				*.cpp)	mv -- "$pathname" "${pathname%.cpp}.c"
+				*.cpp)	new_pathname="${pathname%.cpp}.c"
+						mv -- "$pathname" "$new_pathname"
+						chmod 444 "$new_pathname"
 						number_of_cpp_files=`expr $number_of_cpp_files + 1`;;
 			esac
 		fi
 	done
 }
 
-directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # From https://stackoverflow.com/a/246128
 
 walk_dir "$directory"
 
