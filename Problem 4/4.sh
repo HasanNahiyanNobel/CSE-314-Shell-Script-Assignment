ae4f9d798cdeaec30631cb919269e29a69b1199b
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-03-24 10:49:54 +0600

    Problem 4 works, with a bug which causes deletes the comma after guestname.

--- Problem 4/4.sh
+++ Problem 4/4.sh
@@ -4,21 +4,28 @@ template_file="template.txt"
 guests_list_file="guests.txt"
 template_text=""
 
-# Read from template file
-{
-    read
-    while IFS=, read -r a_line
-    do 
-        template_text="${template_text}\n$a_line"
-    done
-} < $template_file
+## Read from template file
+#{
+#	while IFS=, read -r a_line
+#	do
+#		template_text="${template_text}\n$a_line"
+#	done
+#} < $template_file
 
 
 
 # Create invitation files
 while read -r line; do
-    name="$line"
-    echo -en "" > "invite_$name.txt" # Clear the file    
-    echo -en "Dear $name,$template_text" >> "invite_$name.txt" # Append the text
+	guestname="$line"
+	echo -en "" > "invite_$guestname.txt" # Clear the file	
+	# Append each line of template text to invitation file
+	{
+		while IFS=, read -r template_line
+		do
+			changed_line="${template_line/<guestname>/$guestname}" # Change the line if it contains <guestname>
+			echo $changed_line >> "invite_$guestname.txt" # Append the line to file
+		done
+	} < $template_file
+	# echo -en "Dear $guestname,$template_text" >> "invite_$guestname.txt" # Append the text
 done < "$guests_list_file"
 
