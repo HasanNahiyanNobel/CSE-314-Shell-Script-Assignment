1512f8a788be2b32d3a392a3d82157f7d8da8da2
Hasan Nahiyan Nobel <1405037.hn@ugrad.cse.buet.ac.bd>
2021-03-24 11:12:06 +0600

    Fixed a bug. Solution seems to work perfectly.

--- Problem 4/4.sh
+++ Problem 4/4.sh
@@ -2,30 +2,18 @@
 
 template_file="template.txt"
 guests_list_file="guests.txt"
-template_text=""
 
-## Read from template file
-#{
-#	while IFS=, read -r a_line
-#	do
-#		template_text="${template_text}\n$a_line"
-#	done
-#} < $template_file
-
-
-
-# Create invitation files
-while read -r line; do
+# Loop through the file containing guest names
+while read -r line
+do
 	guestname="$line"
-	echo -en "" > "invite_$guestname.txt" # Clear the file	
-	# Append each line of template text to invitation file
-	{
-		while IFS=, read -r template_line
-		do
-			changed_line="${template_line/<guestname>/$guestname}" # Change the line if it contains <guestname>
-			echo $changed_line >> "invite_$guestname.txt" # Append the line to file
-		done
-	} < $template_file
-	# echo -en "Dear $guestname,$template_text" >> "invite_$guestname.txt" # Append the text
+	echo -en "" > "invite_$guestname.txt" # Clear the file (-en means echo would not insert a new line)
+	
+	# Loop through the template file and look for <guestname> in each line
+	while read -r template_line
+	do
+		changed_line="${template_line/<guestname>/$guestname}" # Change the line if it contains <guestname>
+		echo $changed_line >> "invite_$guestname.txt" # Append the line to file
+	done < "$template_file"
 done < "$guests_list_file"
 
